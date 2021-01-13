02b7ed5a4b07968c3bb046c700cf089bb89851ad
qodbtn41 <qodbtn41@naver.com>
2021-01-13 23:13:56 +0900

    초기 프로젝트 생성

---
+++ jpa-sample/src/test/java/com/example/jpasample/JpaSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpasample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
