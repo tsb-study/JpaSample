25f5e0a398ec9cffc6bf1be89e8ea2c27d1a9a98
qodbtn41 <qodbtn41@naver.com>
2021-01-26 01:55:27 +0900

    CATEGORY --- CATEGORY

--- jpa-sample/src/main/java/com/example/jpasample/domain/Category.java
+++ jpa-sample/src/main/java/com/example/jpasample/domain/Category.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 import lombok.Getter;
@@ -24,4 +27,11 @@ public class Category {
 
   @OneToMany(mappedBy = "category")
   private List<CategoryItem> items;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "PARENT_ID")
+  private Category parent;
+
+  @OneToMany(mappedBy = "parent")
+  private List<Category> children;
 }
