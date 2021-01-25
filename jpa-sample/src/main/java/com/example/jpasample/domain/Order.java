8cc67cc4df48f76edb72246a9179f4842dde30ed
qodbtn41 <qodbtn41@naver.com>
2021-01-26 01:55:43 +0900

    ORDER 테이블 명 에러

--- jpa-sample/src/main/java/com/example/jpasample/domain/Order.java
+++ jpa-sample/src/main/java/com/example/jpasample/domain/Order.java
@@ -15,6 +15,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 import com.example.jpasample.type.OrderStatus;
 
@@ -24,6 +25,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
+@Table(name = "ORDERS")
 public class Order {
   @Id
   @GeneratedValue
